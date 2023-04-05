beaab9bbdfe2ac2308d6eb1745dc0b222f9fcee9
kunyu.li <kunyu.li@appshahe.com>
2023-04-06 00:31:53 +0800

    hhhh

--- assets/main.js
+++ assets/main.js
@@ -6,11 +6,26 @@ $(document).ready(function ()
 		{
 			$(".navigation-wrapper").toggleClass("visible animated bounceOutUp"), $(".navigation-wrapper").off("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend")
 		}), $(".navigation-wrapper").toggleClass("animated bounceInDown animated bounceOutUp")) : $(".navigation-wrapper").toggleClass("visible animated bounceInDown"), $(".btn-mobile-menu__icon").toggleClass("iconfont NianBroken-zhankai iconfont NianBroken-shouqi animated fadeIn")
-	}), $(".navigation-wrapper .blog-button").click(function ()
+	}),
+	$(".navigation-wrapper .blog-button").click(function ()
+
 	{
 		"block" == $(".navigation-wrapper").css("display") && ($(".navigation-wrapper").on("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function ()
 		{
 			$(".navigation-wrapper").toggleClass("visible animated bounceOutUp"), $(".navigation-wrapper").off("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend")
 		}), $(".navigation-wrapper").toggleClass("animated bounceInDown animated bounceOutUp")), $(".btn-mobile-menu__icon").toggleClass("iconfont NianBroken-zhankai iconfont NianBroken-shouqi animated fadeIn")
-	})
+	});
+	$(".panel-cover__logo").click(function(){
+	    console.log("hhhh")
+	    var img_value = $(this).attr("value");
+	    console.log(img_value)
+	    if (img_value == "white"){
+	        $("body").css("background-color","#828986");
+	        $(this).attr("value","gray");
+	    }else{
+	    $("body").css("background-color","white");
+        $(this).attr("value","white");
+	    }
+
+      });
 });
