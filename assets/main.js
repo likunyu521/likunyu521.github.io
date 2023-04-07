e14805577114c39b1e1bd434a510ff4f26eb71b6
kunyu.li <kunyu.li@appshahe.com>
2023-04-07 22:43:08 +0800

    hhh

--- assets/main.js
+++ assets/main.js
@@ -15,7 +15,7 @@ $(document).ready(function ()
 			$(".navigation-wrapper").toggleClass("visible animated bounceOutUp"), $(".navigation-wrapper").off("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend")
 		}), $(".navigation-wrapper").toggleClass("animated bounceInDown animated bounceOutUp")), $(".btn-mobile-menu__icon").toggleClass("iconfont NianBroken-zhankai iconfont NianBroken-shouqi animated fadeIn")
 	});
-	
+
 	$(".panel-cover__logo").click(function(){
 	    console.log("hhhh")
 	    var img_value = $(this).attr("value");
@@ -29,4 +29,18 @@ $(document).ready(function ()
 	    }
 
       });
+      $(document).keyup(function(event){
+             switch(event.keyCode) {
+             case 32:
+             	    var img_value = $(".panel-cover__logo").attr("value");
+             	    if (img_value == "white"){
+             	        $("body").css("background-color","#828986");
+             	        $(".panel-cover__logo").attr("value","gray");
+             	    }else{
+             	    $("body").css("background-color","white");
+                     $(".panel-cover__logo").attr("value","white");
+             return;
+
+             }}
+       });
 });
