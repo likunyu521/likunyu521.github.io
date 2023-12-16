f07fe9f35eee2b5f237b253d1c9aa212f118ccd4
kunyu.li <kunyu.li@appshahe.com>
2023-12-16 20:42:09 +0800

    baidutongji

---
+++ assets/js/baidu_tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?d9604de141bf42895f99f88fceca94ff";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
